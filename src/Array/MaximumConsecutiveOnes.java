925e6a499182fd206c18773f73b8c362b86e79dd
amrit2603 <theamritkumar263@gmail.com>
2026-01-14 23:16:28 +0530

    update

---
+++ src/Array/MaximumConsecutiveOnes.java
@@ -0,0 +1,40 @@
+package Array;
+
+import java.util.*;
+
+class MaximumConsecutiveOnes{
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int cnt = 0;
+        int maxi = 0;
+
+        // Traverse the array
+        for (int i = 0; i < nums.length; i++) {
+
+            // If the current element is 1, increment the count
+            if (nums[i] == 1) {
+                cnt++;
+
+                // Update maxi if current count is greater than maxi
+                maxi = Math.max(maxi, cnt);
+
+            } else {
+                // If the current element is 0, reset the count
+                cnt = 0;
+            }
+        }
+        // Return the maximum count of consecutive 1s
+        return maxi;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 0, 1, 1, 1};
+
+        // Create an instance of the Solution class
+        MaximumConsecutiveOnes sol = new MaximumConsecutiveOnes();
+
+        // Find and print the maximum consecutive 1s
+        int ans = sol.findMaxConsecutiveOnes(nums);
+        System.out.println("The maximum consecutive 1's are " + ans);
+    }
+}
